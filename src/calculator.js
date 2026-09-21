bdf8bf344293e5664ec6bf92ebf29f4f4d74c594
Kevish-Thakkar <kevish.t@shaligraminfotech.com>
2026-09-21 14:34:37 +0530

    refactor: comment out unused mathematical functions in calculator module

--- src/calculator.js
+++ src/calculator.js
@@ -6,30 +6,30 @@ function divide(a, b) {
   return a / b;
 }
 
-function multiply(a, b) {
-  return a * b;
-}
+// function multiply(a, b) {
+//   return a * b;
+// }
 
-function power(a, b) {
-  return Math.pow(a, b);
-}
+// function power(a, b) {
+//   return Math.pow(a, b);
+// }
 // console.log(user.id);
 
 // console.log(user.id);
 
-function subtract(a, b) {
-  return a - b;
-}
+// function subtract(a, b) {
+//   return a - b;
+// }
 
-function additionOfThreeNumbers(a, b, c) {
-  return a + b + c;
-}
+// function additionOfThreeNumbers(a, b, c) {
+//   return a + b + c;
+// }
 
 module.exports = {
   add,
   divide,
-  multiply,
-  power,
-  subtract,
-  additionOfThreeNumbers,
+  // multiply,
+  // power,
+  // subtract,
+  // additionOfThreeNumbers,
 };
